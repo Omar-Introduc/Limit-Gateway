8bb0ff082a7cb0248a7aa6d70ce3286a87051daf
omar-introduc <christian.hermoza.t@uni.pe>
2025-12-04 22:27:43 -0500

    feat:Implementar Pipeline de CI

--- gateway/app/main.py
+++ gateway/app/main.py
@@ -7,12 +7,14 @@ app = FastAPI()
 
 BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8000")
 
+
 @app.get("/")
 def home():
     """
     Root del gateway
     """
     return {"message": "Usted esta en el gateway"}
+
 
 @app.get("/health")
 def health_check():
@@ -21,33 +23,34 @@ def health_check():
     """
     return {"status": "ok"}
 
-#async para que pueda anteder a muchos al mismo tiempo
+
+# async para que pueda anteder a muchos al mismo tiempo
 @app.api_route("/proxy/{path:path}")
 async def proxy(path: str, request: Request):
     """
     Redirige cualquier petición que llegue a /proxy/ hacia el Backend
     """
-    #arma url a partir de path y guarda los parametros de la request
+    # arma url a partir de path y guarda los parametros de la request
     url_destino = f"{BACKEND_URL}/{path}"
     params = dict(request.query_params)
 
-    #httpx se hace pasar por cliente con Asyncclient
+    # httpx se hace pasar por cliente con Asyncclient
     async with httpx.AsyncClient() as client:
-        #manda request a la url y devuelve su respuesta
+        # manda request a la url y devuelve su respuesta
         try:
             proxy_req = await client.request(
-                method=request.method,
-                url=url_destino,
-                params=params,
-                timeout=5.0
+                method=request.method, url=url_destino, params=params, timeout=5.0
             )
             return proxy_req.json()
-            
+
         except httpx.ConnectError:
-            raise HTTPException(status_code=503, detail="No se pudo conectar con el Backend")
+            raise HTTPException(
+                status_code=503, detail="No se pudo conectar con el Backend"
+            )
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e))
 
+
 if __name__ == "__main__":
-    
+
     uvicorn.run("app.main:app", host="0.0.0.0", port=8001, reload=True)
